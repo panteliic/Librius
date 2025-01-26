4f3b68d7a0fd6ca595a77910956dbc1f3b5e8431
Nikola Pantelic <pantelicnikolaa@gamail.com>
2025-01-26 12:30:48 +0100

    routing client app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,8 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./store.ts";
 import App from "./App.tsx";
-import { ClerkProvider } from "@clerk/clerk-react";
+import { BrowserRouter } from "react-router";
+import AppRoutes from "./Routes.tsx";
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
@@ -13,8 +14,8 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
-    </ClerkProvider>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   </Provider>
 );
