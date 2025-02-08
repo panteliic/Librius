7f6baf70038b6d7e7d0d845c41c2d4a0445df4a5
Nikola Pantelic <pantelicnikolaa@gamail.com>
2025-02-08 12:09:48 +0100

    added redora logo

--- frontend/src/components/Message.tsx
+++ frontend/src/components/Message.tsx
@@ -1,5 +1,5 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
-
+import redoraIcon from "../assets/redora.webp"
 interface Props {
   sender: boolean;
 }
@@ -8,7 +8,7 @@ function Message(props: Props) {
   return (
     <div className={`max-w-4/5 flex gap-4 flex-col ${props.sender ? "  self-end" : ""}`}>
       <Avatar className={`w-8 h-8 rounded ${props.sender && " self-end"}`}>
-        <AvatarImage src="" className=" rounded-md" />
+        <AvatarImage src={props.sender ? "": redoraIcon} className=" rounded-md" />
         <AvatarFallback className="bg-primary text-primary-foreground rounded-md">
           NP
         </AvatarFallback>
