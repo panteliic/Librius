90c9557c709229b6604585271ec64b3e42341297
Nikola Pantelic <pantelicnikolaa@gamail.com>
2025-02-15 17:52:26 +0100

    endpoint favorites books

--- backend/src/routes/books.routes.ts
+++ backend/src/routes/books.routes.ts
@@ -1,11 +1,11 @@
 import { addFavorite } from "../controllers/Books/addFavorites";
 import { bookInfo } from "../controllers/Books/bookInfo.controller";
+import { favorites } from "../controllers/Books/favoritesBooks";
 import {
   getFeaturedBooks,
   updateFeaturedBooks,
 } from "../controllers/Books/featuerdBooks.controller";
 import { BookController } from "../controllers/Books/migratebooks.controller.";
-import { optionalAuthMiddleware } from "../middleware/optionalAuthMiddleware";
 import passport from "../passport";
 const express = require("express");
 const router = express.Router();
@@ -14,6 +14,7 @@ router.get("/api/featured-books/:userId", getFeaturedBooks);
 router.get("/api/book-info/:id", bookInfo);
 router.get("/api/migrate-books", BookController.migrateBooks);
 router.post("/api/update-featured-books", updateFeaturedBooks);
-router.post("/api/add-favorites", addFavorite);
+router.post("/api/add-favorites",passport.authenticate("jwt", { session: false }), addFavorite);
+router.get("/api/favorites",passport.authenticate("jwt", { session: false }), favorites);
 
 export default router;
--- frontend/src/pages/FavortiesBook.tsx
+++ frontend/src/pages/FavortiesBook.tsx
@@ -1,10 +1,53 @@
+import { BookCard } from "@/components/Books/BookCard";
+import { RootState } from "@/store/store";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+interface Book {
+  id: number;
+  title: string;
+  authors: string;
+  description: string;
+  category: string;
+  thumbnail: string | null;
+  isFavorite: boolean;
+}
 function Favorites() {
-    return (
-      <div>
-        Favorites
-      </div>
-    )
-  }
-  
-  export default Favorites;
-  
+  const [books, setBooks] = useState<Book[]>([]);
+  const user = useSelector((state: RootState) => state.user.user);
+  useEffect(() => {
+    const fetchFeaturedBooks = async () => {
+      try {
+        const response = await fetch(
+          `${import.meta.env.VITE_API_URL}/favorites`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+          }
+        );
+
+        const result = await response.json();
+
+        setBooks(result.favoriteBooks);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    if (user !== undefined) {
+      fetchFeaturedBooks();
+    }
+  }, [user]);
+  return (
+    <div className="flex justify-start gap-2 flex-wrap h-full overflow-y-auto ScrollBar p-5 pb-20">
+      {books &&
+        books.map((book: Book, index: number) => (
+          <BookCard key={index} {...book} />
+        ))}
+    </div>
+  );
+}
+
+export default Favorites;
